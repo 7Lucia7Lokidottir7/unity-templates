9a6f723604326493426d0b99f6859e18f504e0c4
DESKTOP-88LO54M\Bordi <Bordiyan Georgii>
2025-07-23 12:48:29 +0400

    Improved health system. Added GameOver panel.

---
+++ Templates/Assets/Health/Runtime/GameOver.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PG.HealthSystem
+{
+    public class GameOver : MonoBehaviour, IDeath
+    {
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private float _transitionDuration = 0.25f;
+        [SerializeField] private Selectable _targetSelectable;
+
+        public event Action dead;
+
+        public void OnDeath()
+        {
+            _canvasGroup.gameObject.SetActive(true);
+            _canvasGroup.interactable = true;
+            _canvasGroup.blocksRaycasts = true;
+            _targetSelectable.Select();
+            Time.timeScale = 0;
+            for (float i = 0; i < _transitionDuration; i+= Time.unscaledDeltaTime)
+            {
+                _canvasGroup.alpha = Mathf.Lerp(0, 1f, i / _transitionDuration);
+            }
+            _canvasGroup.alpha = 1f;
+        }
+    }
+}
---
+++ Templates/Assets/Health/Runtime/Reaction/DeathAnimatorReactionController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace PG.HealthSystem
+{
+    public class DeathAnimatorReactionController : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _deathState = "Base Layer.Death";
+        private int _hashState;
+        [SerializeField] private float _transitionAnimation = 0.25f;
+
+        [SerializeField] private GameObject _deathObject;
+        public IDeath death;
+
+        private void OnValidate()
+        {
+            if (_deathObject.GetComponent<IDamagable>() == null)
+            {
+                _deathObject = null;
+                Debug.LogError("DamagableObject haven't interface IDamagable!");
+            }
+        }
+        private void Awake()
+        {
+            _hashState = Animator.StringToHash(_deathState);
+            _deathObject.TryGetComponent(out death);
+        }
+        private void OnEnable()
+        {
+            death.dead += OnDead;
+        }
+        private void OnDisable()
+        {
+            death.dead -= OnDead;
+        }
+        void OnDead()
+        {
+            _animator?.CrossFadeInFixedTime(_hashState, _transitionAnimation);
+        }
+    }
+}
