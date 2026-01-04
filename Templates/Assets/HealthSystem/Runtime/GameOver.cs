3ae0c8c037497f0298bdc25e40d8ad4a5b7e8a20
Georgii Bordiian <149391650+7Lucia7Lokidottir7@users.noreply.github.com>
2026-01-04 11:00:59 +0400

    HealthSystem has been updated.

--- Templates/Assets/HealthSystem/Runtime/GameOver.cs
+++ Templates/Assets/HealthSystem/Runtime/GameOver.cs
@@ -1,3 +1,4 @@
+using PG.MenuManagement;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,11 +8,15 @@ namespace PG.HealthSystem
     public class GameOver : MonoBehaviour, IDeath
     {
         [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private UIShowHide _showHide;
         [SerializeField] private float _transitionDuration = 0.25f;
         [SerializeField] private Selectable _targetSelectable;
 
         public event Action dead;
-
+        public void RemovePanel()
+        {
+            UIManager.RegisterClose(_canvasGroup.gameObject);
+        }
         public void OnDeath()
         {
             _canvasGroup.gameObject.SetActive(true);
@@ -19,6 +24,7 @@ namespace PG.HealthSystem
             _canvasGroup.blocksRaycasts = true;
             _targetSelectable.Select();
             Time.timeScale = 0;
+            UIManager.RequestOpen(_canvasGroup.gameObject);
             for (float i = 0; i < _transitionDuration; i+= Time.unscaledDeltaTime)
             {
                 _canvasGroup.alpha = Mathf.Lerp(0, 1f, i / _transitionDuration);
